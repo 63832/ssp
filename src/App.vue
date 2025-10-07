3ecef16dab090bc61bce86a44d5aba160ecd4351
63832 <153898686+jhamalainen0@users.noreply.github.com>
2025-10-07 11:04:23 +0300

    Gjort att återställningsknappen fungerar via props

--- src/App.vue
+++ src/App.vue
@@ -8,22 +8,14 @@ const knappar = ref(['Sten', 'Sax', 'Påse'])
 const score = ref({ spelare: 0, dator: 0 })
 const resultat = ref({})
 const vinnare = ref('')
+const reset = ref(true)
 
 function hittaVinnare(valdaKnappar) {
+  reset.value = false
   vinnare.value = ''
   let spelare = knappar.value.indexOf(valdaKnappar.spelare)
   let dator = knappar.value.indexOf(valdaKnappar.dator)
   resultat.value = { spelare: spelare, dator: dator }
-}
-
-function reset() {
-  score.value.spelare = 0
-  score.value.dator = 0
-  let buttons = document.getElementsByClassName('alternativ')
-  for (let b of buttons) {
-    b.classList.remove('spelarval')
-    b.classList.remove('datorval')
-  }
 }
 
 function raknaPoang(v) {
@@ -42,11 +34,11 @@ function raknaPoang(v) {
   </header>
 
   <main>
-    <KnappRad :knappar="knappar" @valda-knappar="hittaVinnare" />
-    <ResultatRad :valda-knappar="resultat" @vinnare="raknaPoang" />
-    <PoangRad :vinnare="vinnare" />
+    <KnappRad :knappar="knappar" @valda-knappar="hittaVinnare" :reset="reset" />
+    <ResultatRad :valda-knappar="resultat" @vinnare="raknaPoang" :reset="reset" />
+    <PoangRad :vinnare="vinnare" :reset="reset" />
 
-    <button id="nolla" @click="reset">Nollställ poäng</button>
+    <button id="nolla" @click="reset = true">Nollställ poäng</button>
   </main>
 </template>
 
@@ -57,6 +49,9 @@ header {
 }
 
 #nolla {
+  cursor: pointer;
+  margin: auto;
+  display: block;
   margin-top: 2em;
   padding: 0.3em 0.6em;
   font-size: 0.8em;
