8f5b270d4de6f3718550740344807206b2379570
63832 <153898686+jhamalainen0@users.noreply.github.com>
2025-10-07 08:25:26 +0300

    Lagt till emit, funkar igen

--- src/components/KnappRad.vue
+++ src/components/KnappRad.vue
@@ -2,6 +2,7 @@
 import { ref } from 'vue'
 
 const props = defineProps(['knappar'])
+const emit = defineEmits(['valdaKnappar'])
 
 function spelarval(e) {
   let buttons = document.getElementsByClassName('alternativ')
@@ -9,7 +10,8 @@ function spelarval(e) {
     b.classList.remove('spelarval')
   }
   e.target.classList.add('spelarval')
-  datorval()
+
+  emit('valdaKnappar', { spelare: e.target.textContent, dator: datorval() })
 }
 
 function datorval() {
@@ -23,6 +25,7 @@ function datorval() {
       b.title = 'Datorns val'
     }
   }
+  return props.knappar[val]
 }
 </script>
 <template>
